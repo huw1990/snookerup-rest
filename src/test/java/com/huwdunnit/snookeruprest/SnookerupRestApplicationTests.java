1af3c762a3e296936ddfaff6d61f6a238590a280
Huw Carpenter <huwcarpenter@gmail.com>
2024-03-03 15:07:10 +0000

    Fix test by adding testcontainers

--- src/test/java/com/huwdunnit/snookeruprest/SnookerupRestApplicationTests.java
+++ src/test/java/com/huwdunnit/snookeruprest/SnookerupRestApplicationTests.java
@@ -2,9 +2,11 @@ package com.huwdunnit.snookeruprest;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-class SnookerupRestApplicationTests {
+@ActiveProfiles(profiles = "test")
+class SnookerupRestApplicationTests extends BaseIT {
 
 	@Test
 	void contextLoads() {
